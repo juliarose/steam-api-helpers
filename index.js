a13a0e01b2117297204e2757b572d79190744f1f
juliarose <julialunarose@gmail.com>
2021-03-30 21:21:26 -0400

    added getPlayerSummaries

--- index.js
+++ index.js
@@ -107,6 +107,31 @@ module.exports = function createSteamAPI(apiKey) {
     }
     
     /**
+     * Gets player summaries for given steamids.
+     * @memberof SteamAPI
+     * @param {string[]} steamids - Steam IDs.
+     * @param {string} [format='json'] - Format.
+     * @returns {Promise.<PlayerSummary[]>} Resolves with classinfo.
+     */
+    async function getPlayerSummaries(steamids, format = 'json') {
+        const response = await getJSON({
+            method: 'GET',
+            uri: `https://${API_HOSTNAME}/ISteamUser/GetPlayerSummaries/v0002`,
+            qs: {
+                steamids,
+                format,
+                key: apiKey
+            }
+        });
+        
+        if (!response.response) {
+            throw new Error('No response.');
+        }
+        
+        return response.response.players;
+    }
+    
+    /**
      * Gets classinfo for a classid.
      * @memberof SteamAPI
      * @param {string} appid - Appid.
@@ -370,6 +395,7 @@ module.exports = function createSteamAPI(apiKey) {
     
     return {
         request,
+        getPlayerSummaries,
         getAssetClassInfo,
         getAssetClassInfos,
         getBackpack,
@@ -378,6 +404,32 @@ module.exports = function createSteamAPI(apiKey) {
         getTradeHistory
     };
 }
+
+/**
+ * Player summary.
+ * @typedef {object} PlayerSummary
+ * @property {string} steamid - 64bit SteamID of the user.
+ * @property {string} personaname - The player's persona name (display name).
+ * @property {string} profileurl - The full URL of the player's Steam Community profile.
+ * @property {string} avatar - The full URL of the player's 32x32px avatar. If the user hasn't configured an avatar, this will be the default ? avatar.
+ * @property {string} avatarmedium - The full URL of the player's 64x64px avatar. If the user hasn't configured an avatar, this will be the default ? avatar.
+ * @property {string} avatarfull - The full URL of the player's 184x184px avatar. If the user hasn't configured an avatar, this will be the default ? avatar.
+ * @property {number} personastate - The user's current status. 0 - Offline, 1 - Online, 2 - Busy, 3 - Away, 4 - Snooze, 5 - looking to trade, 6 - looking to play. If the player's profile is private, this will always be "0", except is the user has set his status to looking to trade or looking to play, because a bug makes those status appear even if the profile is private.
+ * @property {number} communityvisibilitystate - This represents whether the profile is visible or not, and if it is visible, why you are allowed to see it. Note that because this WebAPI does not use authentication, there are only two possible values returned: 1 - the profile is not visible to you (Private, Friends Only, etc), 3 - the profile is "Public", and the data is visible. Mike Blaszczak's post on Steam forums says, "The community visibility state this API returns is different than the privacy state. It's the effective visibility state from the account making the request to the account being viewed given the requesting account's relationship to the viewed account."
+ * @property {number} profilestate - If set, indicates the user has a community profile configured (will be set to '1')
+ * @property {number} lastlogoff - The last time the user was online, in unix time.
+ * @property {number} commentpermission - If set, indicates the profile allows public comments.
+ * @property {string} [realname] - The player's "Real Name", if they have set it.
+ * @property {string} [primaryclanid] - The player's primary group, as configured in their Steam Community profile.
+ * @property {number} [timecreated] - The time the player's account was created.
+ * @property {string} [gameid] - If the user is currently in-game, this value will be returned and set to the gameid of that game.
+ * @property {string} [gameserverip] - The ip and port of the game server the user is currently playing on, if they are playing on-line in a game using Steam matchmaking. Otherwise will be set to "0.0.0.0:0".
+ * @property {string} [gameextrainfo] - If the user is currently in-game, this will be the name of the game they are playing. This may be the name of a non-Steam game shortcut.
+ * @property {number} [cityid] - This value will be removed in a future update (see loccityid).
+ * @property {string} [loccountrycode] - If set on the user's Steam Community profile, The user's country of residence, 2-character ISO country code.
+ * @property {number} [locstatecode] - If set on the user's Steam Community profile, The user's state of residence.
+ * @property {number} [loccityid] - An internal code indicating the user's city of residence. A future update will provide this data in a more useful way.
+ */
 
 /**
  * Definition for an item class.
